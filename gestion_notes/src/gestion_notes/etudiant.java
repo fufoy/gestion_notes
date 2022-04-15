65d4f4dba20b7eaceeb34274a66900ac19fcfbdb
p <p@DESKTOP-P99MI0U>
2022-04-15 16:10:04 +0000

    inital commit

--- gestion_notes/src/gestion_notes/etudiant.java
+++ gestion_notes/src/gestion_notes/etudiant.java
@@ -1,28 +1,15 @@
 package gestion_notes;
 
+
 public class etudiant{
-	private int id; 
+	private int id;
 	private String nom;
 	private double note;
 	
-	public etudiant(int id,String nom) {
-		this.id=id;
-		this.nom=nom;
-		this.note=10;
-		
-	}
-	public String toString()
-	{
-		return this.nom+ " : "+this.note; 
-		
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (! (obj instanceof etudiant) ){
-			return false;
-		}
-	return	((etudiant)obj).id==this.id; 
-	}
-	
-}
 
+	
+	
+
+	
+
+}
